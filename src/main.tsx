a3a93abc48a714f67c66ffca7a7cb5494625457b
Mai-Thanh-Thuan-15-4-2 <20130127@st.hcmuaf.edu.vn>
2026-04-19 12:20:29 +0700

    haha

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { registerSW } from 'virtual:pwa-register'
 import './index.css'
 import App from './App.tsx'
 
 if ('serviceWorker' in navigator) {
   if (import.meta.env.PROD) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/sw.js', { scope: '/' })
-        .then((reg) => console.log('SW registration successful with scope: ', reg.scope))
-        .catch((err) => console.log('SW registration failed: ', err));
-    });
+    registerSW({ immediate: true })
   } else {
     navigator.serviceWorker.getRegistrations().then((registrations) => {
       registrations.forEach((registration) => {
